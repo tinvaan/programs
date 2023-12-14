7865f7a4b818f7db2ccf258fca9a5dda0cea885c
Harish Navnit <harishnavnit@gmail.com>
2023-12-14 20:29:08 +0800

    Modular inverse
    Ref: https://stackoverflow.com/questions/4798654/modular-multiplicative-inverse-function-in-python

---
+++ modinv.py
@@ -0,0 +1,21 @@
+
+# pylint: disable-all
+
+
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+
+
+if __name__ == '__main__':
+    assert modinv(5, 7) == pow(5, -1, 7) == 3
